4e87f34d4f9d990d8b6d203ede9baba22b0984c4
Yahong <yahongding1028@gmail.com>
2014-02-24 20:22:17 -0600

    commit 1

---
+++ default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="default.aspx.vb" Inherits="_default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
+
+    <title>Calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>Salary Calculator</h1>
+        <h2>Hourly Wage :</h2>
+        <h3><asp:TextBox ID="tbHoursWage" runat="server"></asp:TextBox></h3>
+        <h2>Number of hours worked this week:</h2>
+        <h3><asp:TextBox ID="tbHoursWorked" runat="server"></asp:TextBox></h3>
+        <h2>Pre-tax Deductions :</h2>
+        <h3 ><asp:TextBox ID="tbPreTax" runat="server" Width="128px"></asp:TextBox></h3>
+        <h2>After Tax Deductions :</h2>
+        <h3><asp:TextBox ID="tbAfterTax" runat="server"></asp:TextBox></h3>
+        <h3><asp:Button ID="Button1" runat="server" Text="Clear" Width="62px" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" /></h3>
+        <h1>Your Net Wage is:
+        <asp:Label ID="lbNetResult" runat="server"></asp:Label></h1>
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
+
---
+++ default.aspx.vb
@@ -0,0 +1,61 @@
+﻿
+Partial Class _default
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        'specify constant valus
+        Const lowTaxRate As Integer = 18
+        Const highTaxRate As Integer = 22
+
+
+        'set condition
+        If tbHoursWage.Text = "" Then
+            Me.lbNetResult.Text = ""
+            MsgBox("Please enter your hourly wage", MsgBoxStyle.Information, "Error Detected in Input")
+        ElseIf tbHoursWorked.Text = "" Then
+            Me.lbNetResult.Text = ""
+            MsgBox("Please enter how many hours you worked", MsgBoxStyle.Information, "Error Detected in Input")
+        ElseIf tbPreTax.Text = "" Then
+            Me.lbNetResult.Text = ""
+            MsgBox("Please enter the pretax deduction", MsgBoxStyle.Information, "Error Detected in Input")
+        ElseIf tbAfterTax.Text = "" Then
+            Me.lbNetResult.Text = ""
+            MsgBox("Please fill up all the textbox", MsgBoxStyle.Information, "Error Detected in Input")
+
+        Else
+            'create variables to hold the values entered by the user
+            Dim hw As Decimal = tbHoursWage.Text
+            Dim hr As Decimal = tbHoursWorked.Text
+            Dim preTaxDed As Decimal = tbPreTax.Text
+            Dim afterTaxDed As Decimal = tbAfterTax.Text
+
+            Dim totalWage As Decimal
+            totalWage = hw * hr
+            Dim netResult As Decimal
+            'calculate the net pay result for the user
+            If totalWage > 500 Then
+                netResult = (totalWage - preTaxDed) * (1 - highTaxRate / 100) - afterTaxDed
+            Else
+                netResult = (totalWage - preTaxDed) * (1 - lowTaxRate / 100) - afterTaxDed
+            End If
+            'checking result
+            If netResult >= 0 Then
+                'display the result in the results label web control
+                lbNetResult.Text = " Your net pay in this week is $ " & netResult.ToString("#,####.##")
+            Else
+                MsgBox("Please check your input since your net wage is less than 0", _
+                       MsgBoxStyle.Information, "Error Detected in Input")
+            End If
+        End If
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'clear textbox for user
+        Me.tbAfterTax.Text = ""
+        Me.tbHoursWage.Text = ""
+        Me.tbPreTax.Text = ""
+        Me.tbHoursWorked.Text = ""
+        Me.lbNetResult.Text = ""
+    End Sub
+End Class
